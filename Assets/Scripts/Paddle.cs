85658b026b2802ac917d85cd3f39f3f22df9b419
AntonMikhailets <antonMikhailets56@gmail.com>
2024-09-06 17:12:08 +0300

    Add base paddle logic

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    private const  string Axis = "Horizontal";
-    
+    private const  string Horizontal = "Horizontal";
+
+    [SerializeField] private RectTransform _rect;
+    [SerializeField] private RectTransform _canvasRect;
     [SerializeField] private float _speed = 10f;
+
+    private float ScreenWidth => _canvasRect.sizeDelta.x;
+    private float PaddleWidth => _rect.sizeDelta.x;
+    private float LeftBorder => -ScreenWidth / 2 + PaddleWidth / 2;
+    private float RightBorder => ScreenWidth / 2 - PaddleWidth / 2;
 
     private void Update()
     {
-        var moveVector = Input.GetAxis(Axis) * _speed * Time.deltaTime;
-        transform.position += new Vector3(moveVector, 0, 0);
+        var moveInput = Input.GetAxis(Horizontal);
+        var newPosition = _rect.localPosition + new Vector3(moveInput * _speed * Time.deltaTime, 0, 0);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, LeftBorder, RightBorder);
+        _rect.localPosition = newPosition;
     }
 }
